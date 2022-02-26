6967caac11a99c92d311a3cb3c1f1af00683ae25
reviser <DMHGyuh@gmail.com>
2022-02-26 13:22:52 -0500

    website update yuh

--- js/app.js
+++ js/app.js
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!!  Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own ${COLLECTION_NAME}!! 💰 Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} ${renderDots(dots)}`;
+      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME}⏱ ${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME} so welcome to the Yuh club!!   `;
+      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME} so welcome to the Yuh club™!!   `;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} ew cringe JK `;
+      statusText.innerText = `You don't own any ${COLLECTION_NAME} ew cringe🤮 JK`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
